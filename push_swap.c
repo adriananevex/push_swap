cebd0c84ea4b682863016412d14afdd70b1d1377
Adriana Neves <nevesaera@outlook.com>
2026-01-26 23:11:54 +0000

    ps 26.01

--- push_swap.c
+++
@@ -1,9 +0,0 @@
-#include "../../inc/push_swap.h"
-
-in main(int argc, char **argv)
-{
-  t_stack_node  *a;
-  t_stack_node  *b;
-
-  a
-}
---
+++ srcs/push_swap/stack_init.c
@@ -0,0 +1,70 @@
+#include "../../inc/push_swap.h"
+
+static long ft_atol(const char *s)
+{
+    long result;
+    int sign;
+    int i;
+
+    result = 0;
+    sign = 1;
+    i = 0;
+    while (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' || \
+            s[i] == '\r' || s[i] == '\f' || s[i] == '\v')
+        i++;
+    if (s[i] == '-' || s[i] == '+')
+    {
+        if (s[i] == '-')
+            sign = -1;
+        i++;
+    }
+    while (ft_isdigit(s[i]))
+        result = result * 10 + (s[i++] - '0');
+    return (result * sign);
+}
+
+static void append_node(t_stack_node **stack, int n)
+{
+    t_stack_node    *node;
+    t_stack_node    *last_node;
+
+    if (!stack)
+        return ;
+    node = malloc(sizeof(t_stack_node))
+    if (!node)
+        return ;
+    node ->next = NULL;
+    node ->nbr = n;
+    node->cheapest = 0;
+    if (!(*stack))
+    {
+        *stack = node;
+        node ->prev = NULL;
+    }
+    else
+    {
+        last_node = find_last(*stack);
+        last_node->next = node;
+        node->prev = last_node;
+    }
+}
+
+void    init_stack_a(t_stack_node **a, char **argv)
+{
+    long    n;
+    int     i;
+
+    i = 0;
+    while (argv[i])
+    {
+        if (error_syntax(argv[i]))
+            free_errors(a);
+        n = ft_atol(argv[i]);
+        if (n > INT_MAX || n < INT_MIN)
+            free_errors(a);
+        if (error_duplicate(*a, (int)n))
+            free_errors(a);
+        append_node(a, (int)n);
+        i++;
+    }
+}
