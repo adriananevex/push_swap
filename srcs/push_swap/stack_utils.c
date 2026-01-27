4ddcce418e30790bf05ff5ad1be12a518e26d65e
Adriana Neves <nevesaera@outlook.com>
2026-01-27 23:43:55 +0000

    ps 2

---
+++ srcs/push_swap/stack_utils.c
@@ -0,0 +1,47 @@
+#include "../../inc/push_swap.h"
+
+int stack_len(t_stack_node *stack)
+{
+    int count;
+
+    if (!stack)
+        return (0);
+    count = 0;
+    while (stack)
+    {
+        stack = stack->next;
+        count++;
+    }
+    return (count);
+}
+
+t_stack_node    *find_last(t_stack_node *stack)
+{
+    if (!stack)
+        return (NULL);
+    while (stack->next)
+        stack = stack->next;
+    return (stack);
+}
+
+bool    stack_sorted(t_stack_node *stack)
+{
+    if (!stack)
+        return (1);
+    while (stack->next)
+    {
+        if (stack->nbr > stack->next->nbr)
+            return (false);
+        stack = stack->next;
+    }
+    return (true);
+}
+
+t_stack_node *find_min(t_stack_node *stack)
+{
+    long    min;
+    t_stack_node    *min_node;
+
+    if (!stack)
+        return (NULL);\
+}
